ffce7183639eb64e95154e818a137c6b0e213eaa
tul.matvey <tul.matvey@gmail.com>
2024-12-10 15:51:27 +0300

    Add logger

--- notification_svc/internal/pkg/app/init.go
+++ notification_svc/internal/pkg/app/init.go
@@ -5,6 +5,7 @@ import (
 	grpcClient "booking/notification_svc/internal/pkg/api/grpc"
 	deliverysystem "booking/notification_svc/internal/usecases"
 	kafkahandler "booking/notification_svc/pkg/kafka"
+	logger "booking/notification_svc/pkg/logger"
 	"context"
 
 	"github.com/segmentio/kafka-go"
@@ -13,21 +14,26 @@ import (
 type NotificationService struct {
 	deliverySystemClient deliverysystem.IDeliverySystem
 	kafkaReader          *kafka.Reader
+	logger               *logger.Logger
 }
 
 func NewNotificationService() (*NotificationService, error) {
+	logger := logger.Logger{}
+
 	ds, err := grpcClient.NewMockDeliverySystemClient()
 	if err != nil {
 		return nil, err
 	}
 	kafka_cfg := config.LoadKafkaConfig()
 	kafkaReader := kafkahandler.NewKafkaReader(kafka_cfg.KafkaURL, kafka_cfg.Topic, kafka_cfg.GroupID)
-	return &NotificationService{deliverySystemClient: ds, kafkaReader: kafkaReader}, nil
+	return &NotificationService{deliverySystemClient: ds, kafkaReader: kafkaReader, logger: &logger}, nil
 }
 
 func (nsvc *NotificationService) Start(ctx context.Context) error {
+	nsvc.logger.LogInfo("Start hotel svc...")
 	for {
 		if m, err := nsvc.kafkaReader.ReadMessage(context.Background()); err != nil {
+			nsvc.logger.LogError(err)
 			return err
 		} else {
 			if string(m.Value) == "Success" {
@@ -38,5 +44,6 @@ func (nsvc *NotificationService) Start(ctx context.Context) error {
 }
 
 func (nsvc *NotificationService) Close() {
+	nsvc.logger.LogInfo("End hotel svc...")
 	nsvc.kafkaReader.Close()
 }
